20d4d516d4d36c434d4e3d94041059457f487683
arthurshmidt <arthur@arthurshmidt.com>
2023-01-19 23:38:19 -0500

    created tar backup command

--- minecraft_update.py
+++ minecraft_update.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+import os
+import sys
+
 import logging
 import requests
 import scrapy
@@ -5,8 +9,11 @@ from bs4 import BeautifulSoup
 
 # Globals
 URL = "https://www.minecraft.net/en-us/download/server/bedrock"
+ALLOW = "/home/minecraft/server/bedrock-edition/allowlist.json"
+WORLDS = "/home/minecraft/server/bedrock-edition/worlds"
+PROPERTIES = "/home/minecraft/server/bedrock-edition/server.properties"
 
 if __name__ == "__main__":
-    page = requests.get(URL)
-    print(page.status_code)
-    print(page)
+    print("Backing up version: "+sys.argv[1])
+    os.system("tar -zcvpf /home/minecraft/server/bedrock-edition/backups/backup-"+sys.argv[1]+".tar.gz "+ALLOW+" "+WORLDS+" "+PROPERTIES)
+#    os.system("tar -zcvpf backup-"+sys.argv[1]+".tar.bz2 README.md") 
