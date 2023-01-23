7b9bf59b5ef89730da56c6f89e9a88d0dd3ae3cb
arthurshmidt <arthur@arthurshmidt.com>
2023-01-23 12:42:21 -0500

    completed install. Ready for testing

--- minecraft_update.py
+++ minecraft_update.py
@@ -14,21 +14,26 @@ WORLDS = "/home/minecraft/server/bedrock/latest_release/worlds"
 PROPERTIES = "/home/minecraft/server/bedrock/latest_release/server.properties"
 
 def backup(version):
+    print("Backing up latest version")
     os.system("tar -zcvpf /home/minecraft/server/bedrock/backups/backup-bedrock-"+version+".tar.gz "+ALLOW+" "+WORLDS+" "+PROPERTIES)
 
 def cleanup():
-    os.system("rm -rf /home/minecraft/server/bedrock/temp/*")
-    os.system("cp -rf /home/minecraft/server/bedrock/latest_release/allowlist.json /home/minecraft/server/bedrock/temp")
-    os.system("cp -rf /home/minecraft/server/bedrock/latest_release/server.properties /home/minecraft/server/bedrock/temp")
-    os.system("cp -rf /home/minecraft/server/bedrock/latest_release/worlds /home/minecraft/server/bedrock/temp")
-    _ = input("Enter to Delete latest_release")
+    print("Cleaning up")
+    os.system("rm -vrf /home/minecraft/server/bedrock/temp/*")
+    os.system("cp -vrf /home/minecraft/server/bedrock/latest_release/allowlist.json /home/minecraft/server/bedrock/temp")
+    os.system("cp -vrf /home/minecraft/server/bedrock/latest_release/server.properties /home/minecraft/server/bedrock/temp")
+    os.system("cp -vrf /home/minecraft/server/bedrock/latest_release/worlds /home/minecraft/server/bedrock/temp")
+    _ = input("Press <Enter> to Delete latest_release")
     os.system("rm -rf /home/minecraft/server/bedrock/latest_release/*")
 
 def install(version2):
+    _ = input("Press <Enter> to Install version: "+version2)
     os.system("unzip /home/minecraft/server/bedrock/server_releases/bedrock-server-"+version2+".zip -d /home/minecraft/server/bedrock/latest_release/")
+    os.system("cp -vrf /home/minecraft/server/bedrock/temp/* /home/minecraft/server/bedrock/latest_release")
 
 if __name__ == "__main__":
     print("Backing up version: "+sys.argv[1]+" to "+sys.argv[2])
-#    backup(sys.argv[1])
-#    cleanup()
+    backup(sys.argv[1])
+    cleanup()
     install(sys.argv[2])
+    print("Update Complete")
