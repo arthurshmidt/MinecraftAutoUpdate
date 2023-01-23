afa5054874b03df94e7204a390783fa7ecb52f6a
arthurshmidt <arthur@arthurshmidt.com>
2023-01-23 11:45:04 -0500

    working on cleanup

--- minecraft_update.py
+++ minecraft_update.py
@@ -9,15 +9,20 @@ from bs4 import BeautifulSoup
 
 # Globals
 URL = "https://www.minecraft.net/en-us/download/server/bedrock"
-ALLOW = "/home/minecraft/server/bedrock-edition/allowlist.json"
-WORLDS = "/home/minecraft/server/bedrock-edition/worlds"
-PROPERTIES = "/home/minecraft/server/bedrock-edition/server.properties"
-PROG = "/home/minecraft/server/bedrock-edition/server_releases/bedrock-server-"
+ALLOW = "/home/minecraft/server/bedrock/allowlist.json"
+WORLDS = "/home/minecraft/server/bedrock/worlds"
+PROPERTIES = "/home/minecraft/server/bedrock/server.properties"
+PROG = "/home/minecraft/server/bedrock/server_releases/bedrock-server-"
 
 def backup(version):
-    os.system("tar -zcvpf /home/minecraft/server/bedrock-edition/backups/backup-"+version+".tar.gz "+ALLOW+" "+WORLDS+" "+PROPERTIES+" "+PROG+version+".zip")
+    os.system("tar -zcvpf /home/minecraft/server/bedrock/backups/backup-bedrock-"+version+".tar.gz "+ALLOW+" "+WORLDS+" "+PROPERTIES+" "+PROG+version+".zip")
+
+def cleanup():
+    os.system("rm -rf /home/minecraft/server/bedrock/temp/*")
+    os.system("cp -rf /home/minecraft/server/bedrock/latest_release/allowlist /home/minecraft/server/bedrock/temp")
 
 if __name__ == "__main__":
-    print("Backing up version: "+sys.argv[1])
-    backup(sys.argv[1])
+    print("Backing up version: "+sys.argv[1]+" to "sys.argv[2])
+#    backup(sys.argv[1])
+    cleanup()
 #    os.system("tar -zcvpf backup-"+sys.argv[1]+".tar.bz2 README.md") 
